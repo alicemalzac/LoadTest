29d27ab9af984240e0e68f9ee1a0e3270fc82216
Alice Malzac <alicelinsc.malzac@gmail.com>
2022-06-20 21:41:42 -0300

    first commit

---
+++ ferramentas/locust/locustfile2.py
@@ -0,0 +1,24 @@
+from locust import HttpUser, LoadTestShape, task, between
+
+class TestLocust(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def get_delay_1(self):
+        self.client.get("/delay/1")
+
+class StagesShape(LoadTestShape):
+
+    stages = [
+        {"duration": 60, "users": 100, "spawn_rate": 100},
+    ]
+
+    def tick(self):
+        run_time = self.get_run_time()
+
+        for stage in self.stages:
+            if run_time < stage["duration"]:
+                tick_data = (stage["users"], stage["spawn_rate"])
+                return tick_data
+
+        return None
